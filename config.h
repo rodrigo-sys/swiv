d1bef7ab6f04701b1d4e6b87879640a165339241
Rodrigo Chavez <chavezrodrigo001@gmail.com>
2026-05-18 21:17:09 -0300

    feat(config): increase thumbnail sizes and default
    Add larger thumbnail options (200, 256, 384, 512) and set
    default from 128px to 512px.

--- config.h
+++ config.h
@@ -47,10 +47,10 @@ static const bool ALPHA_LAYER = false;
 #ifdef _THUMBS_CONFIG
 
 /* thumbnail sizes in pixels (width == height): */
-static const int thumb_sizes[] = { 32, 64, 96, 128, 160 };
+static const int thumb_sizes[] = { 32, 64, 96, 128, 160, 200, 256, 384, 512 };
 
 /* thumbnail size at startup, index into thumb_sizes[]: */
-static const int THUMB_SIZE = 3;
+static const int THUMB_SIZE = 8;
 
 #endif
 #ifdef _MAPPINGS_CONFIG
